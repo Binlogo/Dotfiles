5f9ad11ab7bc123b705420df864bb2a215f53ca9
Binlogo <binboy@live.com>
2022-04-19 23:10:33 +0800

    Setup dotter: include fish config files

---
+++ fish/config.fish
@@ -0,0 +1,32 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# Alias
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
+
+set -gx EDITOR nvim
+
+# PATH
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# Rust & Cargo
+set -gx PATH ~/.cargo/bin:$PATH
+
+# Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Starship prompt
+starship init fish | source
